3bf9c815cc94614ddcf6118a2149674758b1c375
Zhiwen Shao <shaozhiwen@sjtu.edu.cn>
2020-04-01 20:30:08 +0800

    Add files via upload

--- dataset/read_disfa_49land.py
+++ dataset/read_disfa_49land.py
@@ -4,10 +4,6 @@ import scipy.io as sio
 reflect_66 = sio.loadmat('reflect_66.mat')
 reflect_66 = reflect_66 ['reflect_66']
 reflect_66 = reflect_66.reshape(reflect_66.shape[1])
-# reflect_66 = reflect_66 -1
-
-# reflect_66_x = 2 * reflect_66
-# reflect_66_y = 2 * reflect_66 + 1
 
 # each line contains labels of 66 landmarks for an image: x1,y1,x2,y2...
 all_imgs_full_land = np.loadtxt('DISFA_combine_1_2_66land.txt')
